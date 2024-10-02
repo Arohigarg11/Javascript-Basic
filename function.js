a3c826453cfd715d467465452c0adb011f6d6799
arohi garg <gargarohi0@gmail.com>
2024-10-02 15:17:26 +0530

    functions

---
+++ function.js
@@ -0,0 +1,21 @@
+// NAMING FUNCTION
+// function add(a,b) {
+//     console.log(a+b))
+    //    return a+b
+// }
+// add(a+b)
+
+//  ANOYNOUMOUS FUNCTION
+// {
+//     function() {
+    // console.log("hello")
+// }}()
+
+// ARROW Function
+// let add = () => {
+//     console.log("hello")
+// }
+// add()
+
+// let add = (a, b) => a + b
+// console.log(add(10,20))
