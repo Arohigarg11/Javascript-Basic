abb71e1df7d708cab0ec7ba8a444bc6395928da2
arohi garg <gargarohi0@gmail.com>
2024-10-02 15:19:53 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,72 @@
+
+let students = [
+    {
+    id: 1,
+    name: "arohi",
+    age: 19,
+    city: "jaipur"
+},
+{
+    id: 2,
+    name:"mohit",
+    age: 29,
+    city: "udaipur"
+    },
+    {
+    id: 3,
+    name:"mohit",
+    age: 49,
+    city: "udaipur"
+    },
+    {
+    id: 4,
+    name:"mohit",
+    age: 9,
+    city: "udaipur"
+    },
+    {
+    id: 5,
+    name:"mohit",
+    age: 36,
+    city: "udaipur"
+    },
+    {
+    id: 6,
+    name:"mohit",
+    age: 76,
+    city: "udaipur"
+    },
+    {
+    id: 7,
+    name:"mohit",
+    age: 16,
+    city: "udaipur"
+    },
+    {
+    id: 8,
+    name:"mohit",
+    age: 91,
+    city: "udaipur"
+    },
+    {
+    id: 9,
+    name:"mohit",
+    age: 39,
+    city: "udaipur"
+    },
+    {
+    id: 10,
+    name:"mohit",
+    age: 10,
+    city: "udaipur"
+    }
+]
+console.log(students)
+let total = students.filter(students => students.age > 18(studets => students.name) ;
+console.log(total))
+//  console.log(student.age, student.name, student.city)
+//  console.log(student["name"], student["age"])
+// console.log(length.student)
+// for (let i of student) {
+//     console.log(i.age<18)
+// }
